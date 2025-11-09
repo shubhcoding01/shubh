9f7950255bc83223ef51244056540e5ba3f075f4
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-11-09 22:15:30 +0530

    Updated paths for Ingenious Prime, Employee-Task-Assigner, SccoffeeShop-code, and Ecommerce project images to reflect new naming conventions.

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -63,7 +63,6 @@
 
 // export default Works;
 
-
 import React from "react";
 import textureImg from "../resources/images/background_texture.png";
 import Star from "../resources/images/star-2.png";
@@ -72,60 +71,71 @@ import "../styles/Works.css";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import { Helmet } from "react-helmet-async";
-import WorkData from "./data/WorkData"; // <-- This is where it pulls the data
+import WorkData from "./data/WorkData";
 import Navbar from "../components/Navbar";
 
 const Works = () => {
-  return (
-    <>
-      <Helmet>
-        <title>Work | {process.env.REACT_APP_APP_NAME}</title>
-      </Helmet>
-      <Navbar/>
-      <section className="project_area">
-        <div className="container">
-          <h1 className="section_heading" data-aos="fade-up">
-            <img src={Star} alt="star" />
-            All Projects
-            <img src={Star} alt="star" />
-          </h1>
-          <div className="row">
-            {/* This loop now only iterates over your 6 projects */}
-            {WorkData.map((work) => (
-              <div
-                className="col-lg-4 col-md-6 col-sm-6"
-                data-aos="zoom-in"
-                key={work.slug}
-              >
-                <div className="project_item shadow_box">
-                  <Link
-                    className="overlay_link"
-                    to={`/work-details/${work.slug}`}
-                  ></Link>
-                  <img src={textureImg} alt="" className="bg_img" />
-                  <div className="project_img">
-                    <img src={work.image} alt="project1" />
-                  </div>
-                  <div className="d-flex align-items-center justify-content-between">
-                    <div className="project_info">
-                      <h1>{work.name}</h1>
-                    </div>
-                    <Link
-                      to={`/work-details/${work.slug}`}
-                      className="project_btn"
-                    >
-                      <img src={nextIcon} alt="Button" />
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-      <Footer />
-    </>
-  );
+  return (
+    <>
+      <Helmet>
+        <title>Work | {process.env.REACT_APP_APP_NAME}</title>
+      </Helmet>
+
+      <Navbar />
+
+      {/* ===== PROJECTS SECTION ===== */}
+      <section className="project_area">
+        <div className="container">
+          <h1 className="section_heading" data-aos="fade-up">
+            <img src={Star} alt="star" />
+            All Projects
+            <img src={Star} alt="star" />
+          </h1>
+
+          <div className="row">
+            {WorkData.map((work) => (
+              <div
+                className="col-lg-4 col-md-6 col-sm-6"
+                data-aos="zoom-in"
+                key={work.slug}
+              >
+                <div className="project_item shadow_box">
+                  {/* Overlay clickable link */}
+                  <Link
+                    className="overlay_link"
+                    to={`/work-details/${work.slug}`}
+                  ></Link>
+
+                  {/* Background texture */}
+                  <img src={textureImg} alt="background texture" className="bg_img" />
+
+                  {/* Project image */}
+                  <div className="project_img">
+                    <img src={work.image} alt={work.name} />
+                  </div>
+
+                  {/* Project Info and Button */}
+                  <div className="d-flex align-items-center justify-content-between">
+                    <div className="project_info">
+                      <h1>{work.name}</h1>
+                    </div>
+                    <Link
+                      to={`/work-details/${work.slug}`}
+                      className="project_btn"
+                    >
+                      <img src={nextIcon} alt="Next Project" />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </>
+  );
 };
 
 export default Works;
